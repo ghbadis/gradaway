06a3fe9cbba2af24509d43b79043d68c696d7ab6
ghbadis <badisghaoui10@gmail.com>
2025-04-25 00:44:20 +0100

    first user commit

--- src/main/java/Services/ServiceUser.java
+++ src/main/java/Services/ServiceUser.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class ServiceUser implements Services.IService<User> {
     private Connection con;
 
+
     public ServiceUser() {
         con = MyDatabase.getInstance().getCnx();
     }
