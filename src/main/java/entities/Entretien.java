0e884cc5dff3b65fe734d475bb3fa60110f7bd84
Your Name <you@example.com>
2025-05-07 17:29:39 +0100

    pushed

--- src/main/java/entities/Entretien.java
+++ src/main/java/entities/Entretien.java
@@ -10,25 +10,34 @@ public class Entretien {
     private LocalDate date_entretien;
     private LocalTime heure_entretien;
     private String etat_entretien;
+    private String type_entretien;
 
-    public Entretien(int id_entretien, int id_expert, int id_user, LocalDate date_entretien, LocalTime heure_entretien, String etat_entretien) {
+    public Entretien(int id_entretien, int id_expert, int id_user, LocalDate date_entretien, LocalTime heure_entretien, String etat_entretien, String type_entretien) {
         this.id_entretien = id_entretien;
         this.id_expert = id_expert;
         this.id_user = id_user;
         this.date_entretien = date_entretien;
         this.heure_entretien = heure_entretien;
         this.etat_entretien = etat_entretien;
+        this.type_entretien = type_entretien;
     }
 
-
-    public Entretien(int id_expert, int id_user, LocalDate date_entretien, LocalTime heure_entretien, String etat_entretien) {
+    public Entretien(int id_expert, int id_user, LocalDate date_entretien, LocalTime heure_entretien, String etat_entretien, String type_entretien) {
         this.id_expert = id_expert;
         this.id_user = id_user;
         this.date_entretien = date_entretien;
         this.heure_entretien = heure_entretien;
         this.etat_entretien = etat_entretien;
+        this.type_entretien = type_entretien;
     }
 
+    public String getType_entretien() {
+        return type_entretien;
+    }
+
+    public void setType_entretien(String type_entretien) {
+        this.type_entretien = type_entretien;
+    }
 
     public int getId_entretien() {
         return id_entretien;
@@ -86,7 +95,8 @@ public class Entretien {
                 ", id_user=" + id_user +
                 ", date_entretien=" + date_entretien +
                 ", heure_entretien=" + heure_entretien +
-                ", etat_entretien='" + etat_entretien + '\'' +
+                ", etat_entretien='" + etat_entretien +
+                ", type_entretien='" + type_entretien +
                 '}';
     }
 }
